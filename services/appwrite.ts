fa8be58e56e5442eb5b2b8bbedd0996513850cc7
Oluwatosin Ategbole <oluwatosinategbole@gmail.com>
2025-11-09 16:23:23 -0600

    fixing appwrite issues

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,13 +1,14 @@
-import { Client, Databases, ID, Query } from "react-native-appwrite";
+import { Account, Client, Databases, ID, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
+const account = new Account(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
   try {
